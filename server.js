5d8c1f4e3c8898d2ca036097e44182c1b57e3a82
CalleAlexanderson <calle.alexanderson@telia.com>
2024-05-16 15:21:57 +0200

    skapat de olika crud, inte testat så de funkar än

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
 
 const staffAcountSchema = new mongoose.Schema({
     username: {
@@ -49,6 +59,40 @@ const menuItemSchema = new mongoose.Schema({
     }
 });
 
+const oldMenuItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
+    menu: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    restrictions: {
+        type: [String],
+        required: true
+    },
+    whoRemoved: {
+        type: String,
+        required: true
+    },
+    whenRemoved: {
+        type: Date,
+        required: true
+    }
+});
+
 const reviewsSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -74,3 +118,162 @@ const reviewsSchema = new mongoose.Schema({
 const staffAcount = mongoose.model("staffAcount", staffAcountSchema);
 const review = mongoose.model("review", reviewsSchema);
 const menuItem = mongoose.model("menuItem", menuItemSchema);
+const oldMenuItem = mongoose.model("oldMenuItem", oldMenuItemSchema);
+
+//logga in, använt samma kod som i labb 4 (anpassad)
+app.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ error: "Invalid input, send accountUserName and password" })
+        } else {
+            //hämtar user
+            const user = await staffAcount.find({ username: username });
+
+            if (user) { //om user finns kollas lösenord
+                if (await bcrypt.compare(password, user[0].password)) {
+
+                    //skapar en JWT
+                    const payload = { username: username }
+                    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '0.5h' })
+                    const respone = {
+                        user: user,
+                        token: token
+                    }
+                    return res.json(respone);
+                } else {
+                    return res.status(400).json({ message: "incorrect accountUserName/password" });
+                }
+            } else {
+                return res.status(400).json({ message: "incorrect accountUserName/password" });
+            }
+        }
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+})
+
+// lägger till något på menyn
+app.post("/addtomenu", authenticateToken, async (req, res) => {
+    try {
+        const { user, name, cost, description, restrictions, menu, category } = req.body;
+
+        if (!name || !cost || !restrictions || !menu || !category) {
+            return res.status(400).json({ error: "Invalid input, send name, cost, restrictions, menu and category" })
+        }
+
+        const item = {
+            "name": name,
+            "cost": cost,
+            "description": description,
+            "restrictions": restrictions,
+            "menu": menu,
+            "category": category,
+            "whoAdded": user,
+            "whenAdded": new Date()
+        }
+
+        let result = await menuItem.insertOne(item);
+        return res.json(result);
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+})
+
+//om JWT är korrekt skickas det tillbaka länk som används på login sidan för att skicka dig vidare
+app.get("/currentmenu", authenticateToken, async (req, res) => {
+    res.json({ protectedSite: "http://localhost:1234/current_menu.html" })
+})
+
+//kollar om JWT är korrekt, samma kod som labb 4
+function authenticateToken(req, res, next) {
+    const authheader = req.headers['authorization'];
+    const token = authheader && authheader.split(' ')[1]; // tar bort bearer
+
+    if (token == null) {
+        res.status(401).json({ message: "not authorized to see this content, pls log in first" })
+    }
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err) => {
+        if (err) {
+            return res.status(403).json({ message: "not correct JWT" })
+        }
+    })
+    next();
+}
+
+//tar bort ett menuItem från menyn och lägger till det i oldMenuItems
+app.delete("/removefrommenu", authenticateToken, async (req, res) => {
+    try {
+        const { user, name, cost, menu, category } = req.body;
+        const item = await menuItem.find({ name: name, cost: cost, menu: menu, category: category });
+
+        if (item) {
+            const oldItem = {
+                "name": item.name,
+                "cost": item.cost,
+                "description": item.description,
+                "restrictions": item.restrictions,
+                "menu": item.menu,
+                "category": item.category,
+                "whoRemoved": user,
+                "whenRemoved": new Date()
+            }
+            oldMenuItem.insertOne(oldItem)
+            let result = await menuItem.deleteOne(item);
+            return res.json(result);
+        } else {
+            return res.status(404).json(err);
+        }
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+})
+
+//Lägger tillbaka ett menuItem som tagits bort från menyn
+app.put("/addbacktomenu", authenticateToken, async (req, res) => {
+    try {
+        const { user, name, cost, menu, category } = req.body;
+        const oldItem = await oldMenuItem.find({ name: name, cost: cost, menu: menu, category: category });
+
+        if (oldItem) {
+            const item = {
+                "name": oldItem.name,
+                "cost": oldItem.cost,
+                "description": oldItem.description,
+                "restrictions": oldItem.restrictions,
+                "menu": oldItem.menu,
+                "category": oldItem.category,
+                "whoAdded": user,
+                "whenAdded": new Date()
+            }
+            menuItem.insertOne(item)
+            let result = await oldMenuItem.deleteOne(oldItem);
+            return res.json(result);
+        } else {
+            return res.status(404).json(err);
+        }
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+})
+
+app.post("/postreview", async (req, res) => {
+    try {
+        const { name, content, stars } = req.body;
+
+        const newReview = {
+            "name": name,
+            "whenMade": new Date(),
+            "content": content,
+            "stars": stars
+        }
+        let result = await review.insertOne(newReview);
+        return res.json(result);
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+})
+
+app.listen(port, () => {
+    console.log("Server is listening");
+})
